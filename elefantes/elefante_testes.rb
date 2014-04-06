a5063efac09ed414294ced48e1a7c7576a1cdc6d
Yan Justino <yanlimaj@gmail.com>
2014-04-06 10:19:32 -0300

    Refatorando testes

--- elefantes/elefante_testes.rb
+++ elefantes/elefante_testes.rb
@@ -2,15 +2,17 @@ require "test/unit"
 require_relative "elefante.rb"
 
 class ElefanteTeste < Test::Unit::TestCase
+  def setup
+    @elefantes = Elefante.new
+  end
+
   def test_exebir_frase_padrao
-    elefante = Elefante.new
-    mensagem = elefante.incomoda?
+    mensagem = @elefantes.incomodam?
     assert_equal mensagem, "1 elefante incomoda muita gente, 2 elefantes incomodam, incomodam muito mais"
   end
 
   def test_exibir_frase_2_elefantes
-    elefante = Elefante.new
-    mensagem = elefante.incomodam? 2
+    mensagem = @elefantes.incomodam? 2
     assert_equal mensagem, "2 elefantes incomodam muita gente, 3 elefantes incomodam, incomodam, incomodam muito mais"
   end
 end
