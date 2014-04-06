a74b001b41b63c0715952ff0992459699cde9f3b
Yan Justino <yanlimaj@gmail.com>
2014-04-06 09:52:01 -0300

    teste exebir frase padrao

--- elefantes/elefante_testes.rb
+++ elefantes/elefante_testes.rb
@@ -2,7 +2,7 @@ require "test/unit"
 require_relative "elefante.rb"
 
 class ElefanteTeste < Test::Unit::TestCase
-  def test_classe_deve_apresentar_frase_padrao
+  def test_apresentar_frase_padrao
     elefante = Elefante.new
     mensagem = elefante.incomoda?
     assert_equal mensagem, "1 elefante incomoda muita gente, 2 elefantes incomodam, incomodam muito mais"
