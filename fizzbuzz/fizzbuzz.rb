6a55ddfc1fa5443a5b270f03371dc957aa718435
Yan Justino <yanlimaj@gmail.com>
2014-04-06 16:53:38 -0300

    contando de 1 a 15

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,17 @@
 class FizzBuzz
-  def contar(numero)
-    if numero == 5
-      'Buzz'
-    elsif numero == 3
-      'Fizz'
-    elsif numero == 15
+  def contar(numeros)
+    numeros.map { |numero| processar_numero(numero) }.join(', ')
+  end
+
+  def processar_numero(numero)
+    if numero % 5 == 0 && numero % 3 == 0
       'FizzBuzz'
+    elsif numero % 3 == 0
+      'Fizz'
+    elsif numero % 5 == 0
+      'Buzz'
+    else
+      numero.to_s
     end
   end
 end
