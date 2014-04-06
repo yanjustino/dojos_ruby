aafee06b98c1bb67165dc7d59aa5261c29cb98f6
Yan Justino <yanlimaj@gmail.com>
2014-04-06 10:19:48 -0300

    Refatorando classe elefante

--- elefantes/elefante.rb
+++ elefantes/elefante.rb
@@ -1,9 +1,11 @@
 class Elefante
-  def incomoda?
-    "1 elefante incomoda muita gente, 2 elefantes incomodam, incomodam muito mais"
-  end
-
-  def incomodam?(number)
-    "2 elefantes incomodam muita gente, 3 elefantes incomodam, incomodam, incomodam muito mais"
+  def incomodam?(number = 1)
+    if number == 1
+      "1 elefante incomoda muita gente, 2 elefantes incomodam, incomodam muito mais"
+    elsif number == 2
+      "2 elefantes incomodam muita gente, 3 elefantes incomodam, incomodam, incomodam muito mais"
+    elsif number == 3
+      "3 elefantes incomodam muita gente, 4 elefantes incomodam, incomodam, incomodam, incomodam muito mais"
+    end
   end
 end
