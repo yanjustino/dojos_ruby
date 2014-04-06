4b1467c0edf920280bc9d2d150bdfa19f48f6e5c
Yan Justino <yanlimaj@gmail.com>
2014-04-06 10:40:07 -0300

    refatorando testes

--- elefantes/elefante_testes.rb
+++ elefantes/elefante_testes.rb
@@ -6,26 +6,26 @@ class ElefanteTeste < Test::Unit::TestCase
     @elefantes = Elefante.new
   end
 
-  def test_exebir_frase_padrao
-    mensagem = @elefantes.incomodam?
-    assert_equal mensagem, "1 elefante incomoda muita gente, 2 elefantes incomodam, incomodam muito mais"
-  end
-
-  def test_exibir_frase_2_elefantes
-    mensagem = @elefantes.incomodam? 2
-    assert_equal mensagem, "2 elefantes incomodam muita gente, 3 elefantes incomodam, incomodam, incomodam muito mais"
-  end
-
-  def test_exibir_frase_3_elefantes
-    mensagem = @elefantes.incomodam? 3
-    assert_equal mensagem, "3 elefantes incomodam muita gente, 4 elefantes incomodam, incomodam, incomodam, incomodam muito mais"
-  end
-
-  def test_exibir_frase_0_or_nil_elefantes
+  def test_exibir_verso_0_or_nil_elefantes
     mensagem_nil = @elefantes.incomodam? nil
     mensagem_zero = @elefantes.incomodam? 0
 
     assert_equal mensagem_nil, "Quando não há elefantes ninguém fica incomodado :D"
     assert_equal mensagem_zero, "Quando não há elefantes ninguém fica incomodado :D"
+  end
+
+  def test_exebir_verso_1_elefante
+    mensagem = @elefantes.incomodam?
+    assert_equal mensagem, "1 elefante incomoda muita gente, 2 elefantes incomodam, incomodam muito mais"
+  end
+
+  def test_exibir_verso_2_elefantes
+    mensagem = @elefantes.incomodam? 2
+    assert_equal mensagem, "2 elefantes incomodam muita gente, 3 elefantes incomodam, incomodam, incomodam muito mais"
+  end
+
+  def test_exibir_verso_3_elefantes
+    mensagem = @elefantes.incomodam? 3
+    assert_equal mensagem, "3 elefantes incomodam muita gente, 4 elefantes incomodam, incomodam, incomodam, incomodam muito mais"
   end
 end
