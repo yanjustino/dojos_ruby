7b63283abfeb9177a76d6a1af9d33654c3e050c8
Yan Justino <yanlimaj@gmail.com>
2014-04-06 10:09:56 -0300

    exibir verso  para 2 elefantes

--- elefantes/elefante.rb
+++ elefantes/elefante.rb
@@ -2,4 +2,8 @@ class Elefante
   def incomoda?
     "1 elefante incomoda muita gente, 2 elefantes incomodam, incomodam muito mais"
   end
+
+  def incomodam?(number)
+    "2 elefantes incomodam muita gente, 3 elefantes incomodam, incomodam, incomodam muito mais"
+  end
 end
--- elefantes/elefante_testes.rb
+++ elefantes/elefante_testes.rb
@@ -8,5 +8,9 @@ class ElefanteTeste < Test::Unit::TestCase
     assert_equal mensagem, "1 elefante incomoda muita gente, 2 elefantes incomodam, incomodam muito mais"
   end
 
-
+  def test_exibir_frase_2_elefantes
+    elefante = Elefante.new
+    mensagem = elefante.incomodam? 2
+    assert_equal mensagem, "2 elefantes incomodam muita gente, 3 elefantes incomodam, incomodam, incomodam muito mais"
+  end
 end
