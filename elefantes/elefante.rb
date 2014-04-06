a29fd081b2c1b6869daa03de53df1493e4e59c3c
Yan Justino <yanlimaj@gmail.com>
2014-04-06 11:32:00 -0300

    refatorando classe elefante

--- elefantes/elefante.rb
+++ elefantes/elefante.rb
@@ -1,11 +1,32 @@
+
 class Elefante
-  def incomodam?(number = 1)
-    if number == 0 || number == nil
+  def incomodam?(quantidade = 1)
+    @quantidade = quantidade
+
+    unless algum_elefante_foi_informado?
       "Quando não há elefantes ninguém fica incomodado :D"
     else
-      elefante_numero = number > 1 ? 'elefantes incomodam' : 'elefante incomoda'
-      incomodam_array = Array.new(number + 1) { 'incomodam' }.join(', ')
-      verso_completo = "#{number} #{elefante_numero} muita gente, #{number + 1} elefantes #{incomodam_array} muito mais"
+      montar_verso!
     end
+  end
+
+  private
+
+  def algum_elefante_foi_informado?
+    @quantidade.to_s.match /[1-9]/
+  end
+
+  def montar_verso!
+    primeira_parte_do_verso + segunda_parte_do_verso
+  end
+
+  def primeira_parte_do_verso
+    conjugacao_elefante = @quantidade > 1 && 'elefantes incomodam' || 'elefante incomoda'
+    "#{ @quantidade } #{ conjugacao_elefante } muita gente, "
+  end
+
+  def segunda_parte_do_verso
+    vezes_que_icomodam = Array.new(@quantidade + 1) { 'incomodam' }.join(', ')
+    "#{ @quantidade + 1 } elefantes #{ vezes_que_icomodam } muito mais"
   end
 end
