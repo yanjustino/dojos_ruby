8082cd55b88888e98076c8c39dfd11ca94d0ff29
Yan Justino <yanlimaj@gmail.com>
2014-04-22 09:21:53 -0300

    refatorando metodos

--- pedra_papel_tesoura/partida.rb
+++ pedra_papel_tesoura/partida.rb
@@ -6,10 +6,21 @@ class Partida
   end
 
   def jogar(jogador1, jogador2)
-    [jogador1, jogador2].permutation do |partida|
-      return partida[0] if @vitorias.include? partida
-    end
+    @partida = [jogador1, jogador2]
 
-    'empate'
+    vencedor_da_partida || 'empate'
+  end
+
+  private
+
+  def vencedor_da_partida
+    unless empate?
+      @partida.permutation.each { |partida|
+        return partida[0] if @vitorias.include? partida }
+    end
+  end
+
+  def empate?
+    @partida[0] == @partida[1]
   end
 end
